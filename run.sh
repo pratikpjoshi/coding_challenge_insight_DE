b896af9387b298aba22decf3d8ce9a395025e5c2
pratikpjoshi <pratikjoshi0105@gmail.com>
2015-07-15 01:07:15 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # Similar to described in example program I will execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
-python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
+python ./src/combined_program.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
+python ./src/combined_program.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
